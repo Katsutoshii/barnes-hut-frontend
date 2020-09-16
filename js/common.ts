4c519d3ee4305c55cc64ec3b857c5cc3fba260a1
Angela He <heangela.arts@gmail.com>
2020-09-16 15:41:25 -0700

    beautify

--- js/common.ts
+++ js/common.ts
@@ -1,12 +1,17 @@
+export const clamp = (num, min, max) =>
+  num <= min ? min : num >= max ? max : num;
+
 export enum Optimization {
   BarnesHut,
-  Regular,
+  Direct,
 }
 
 // How many stars that will be simulated?
-export const DEF_NUM_SIMULATE = 1000;
+export const DEF_NUM_SIMULATE = 200;
+// Below should equal MAX_PARTICLES from lib.rs
 export const MAX_NUM_SIMULATE = 10000;
-export const MAX_NUM_DIRECT_SIMULATE = 2000;
+export const MAX_NUM_DIRECT_SIMULATE = 5000;
+export const MAX_NUM_BARNES_SIMULATE = 10000;
 // How many background stars, not a part of the simulation?
 export const DEF_NUM_STARS = 10000;
 export const MAX_NUM_STARS = 500000;
